33e37f90f1c877a3078d9297988adc5cc2113311
tan <tanmaykm@gmail.com>
2013-03-26 13:28:59 +0530

    tests updated

--- test/time_test.jl
+++ test/time_test.jl
@@ -3,47 +3,88 @@ using Test
 
 function compare_str_dict()
     println("comparing JudySL with Dict{String, Int64}")
+    #const nloops = 10000000
+    const nloops = 100000
+    local x::Int64 = 0
 
-    const nloops = 10000000
-
-    println("Dict{String, Int64}...")
     d = Dict{String, Int64}()
-    @time for i in 1:nloops
-        d[string(i)] = i
+    gc_disable()
+    #println("inserts: Dict{String, Int64}...")
+    dict_ins = @elapsed for i in 1:nloops
+        d[repeat(string(i), 10)] = i
     end
+    gc_enable()
+    gc()
+    x = 0
+    #println("access Dict{String, Int64}...")
+    dict_access = @elapsed for i in 1:nloops
+        x += d[repeat(string(i), 10)]
+    end
+    gc_enable()
     d = Nothing
     gc()
 
-    println("JudySL...")
     ja = JudySL()
-    @time for i in 1:nloops
-        ja[string(i)] = i
+    gc_disable()
+    #println("inserts JudySL...")
+    ja_ins = @elapsed for i in 1:nloops
+        ja[repeat(string(i), 10)] = i
     end
+    gc_enable()
+    gc()
+    #println("access JudySL...")
+    ja_access = @elapsed for i in 1:nloops
+        x += ja[repeat(string(i), 10)]
+    end
+    gc_enable()
     ja = Nothing
     gc()
+    println("inserts  => dict: ", dict_ins, ", judy: ", ja_ins);
+    println("accesses => dict: ", dict_access, ", judy: ", ja_access);
 end
 
 function compare_int64_dict()
     println("comparing JudyL with Dict{Int64, Int64}")
     const nloops = 10000000
+    local x::Int64 = 0
 
-    println("Dict{Int64, Int64}...")
     d = Dict{Int64, Int64}()
-    @time for i in 1:nloops
+    gc_disable()
+    #println("inserts: Dict{Int64, Int64}...")
+    dict_ins = @elapsed for i in 1:nloops
         d[i] = i
     end
+    gc_enable()
+    gc()
+    x = 0
+    #println("access: Dict{Int64, Int64}...")
+    dict_access = @elapsed for i in 1:nloops
+        x += d[i]
+    end
+    gc_enable()
     d = Nothing
     gc()
 
-    println("JudyL...")
     ja = JudyL()
-    @time for i in 1:nloops
+    gc_disable()
+    #println("inserts: JudyL...")
+    ja_ins = @elapsed for i in 1:nloops
         ja[i] = i
     end
+    gc_enable()
+    gc()
+    x = 0
+    #println("access: JudyL")
+    ja_access = @elapsed for i in 1:nloops
+        x += ja[i]
+    end
+    gc_enable()
     ja = Nothing
     gc()
-
+    println("inserts  => dict: ", dict_ins, ", judy: ", ja_ins);
+    println("accesses => dict: ", dict_access, ", judy: ", ja_access);
 end
 
 compare_int64_dict()
 compare_str_dict()
+
